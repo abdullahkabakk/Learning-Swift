36e50dc3eb90109478b11df6553733a25b2a644e
Abdullah Kabak <05200000101@ogrenci.ege.edu.tr>
2023-03-28 20:58:49 +0300

    Done Creating and Combining Views in SwiftUI Tutorials

---
+++ Landmarks/Landmark.swift
@@ -0,0 +1,8 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Abdullah Kabak on 28.03.2023.
+//
+
+import Foundation
---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Abdullah Kabak on 28.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
