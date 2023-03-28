e08332d1b40948f98f406c5f4d15421b1d68c952
Abdullah Kabak <05200000101@ogrenci.ege.edu.tr>
2023-03-28 23:40:01 +0300

    Done Building Lists and Navigation in SwiftUI

--- Landmarks/Views/CircleImage.swift
+++ Landmarks/Views/CircleImage.swift
@@ -8,15 +8,20 @@
 import SwiftUI
 
 struct CircleImage: View {
+    var image: Image
+    
     var body: some View {
-        Image("turtlerock").clipShape(Circle()).overlay {
+        image
+            .clipShape(Circle())
+            .overlay {
             Circle().stroke(.white, lineWidth: 4)
-        }.shadow(radius: 7)
+        }
+            .shadow(radius: 7)
     }
 }
 
 struct CircleImage_Previews: PreviewProvider {
     static var previews: some View {
-        CircleImage()
+        CircleImage(image: Image("turtlerock"))
     }
 }
---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Abdullah Kabak on 28.03.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
